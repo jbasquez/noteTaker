bf189e153aac51567b8147a6e01b69b9243cfdf6
Joe Basquez <jybasquez@yahoo.com>
2021-02-24 22:32:36 -0500

    added in post

--- server.js
+++ server.js
@@ -26,7 +26,18 @@ app.get('/notes', (req, res) => res.sendFile(path.join("./public/notes.html")));
 app.get('/api/notes', (req, res) => res.json(notes));
 
 // app.post('/api/notes'
-
+app.post('/api/notes', (req, res) => {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    const newNote = req.body;
+ 
+    newNote.routeName = newNote.name.toLowerCase();
+    console.log(newNote);
+  
+    noteListItems.push(newNote);
+    res.json(newNote);
+  });
+  
 //app.delete('/api/not') CRUD
 
 // Starts the server to begin listening
